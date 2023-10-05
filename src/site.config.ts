51ac61b1f4d09ec237095c72abb384e1ee2873d7
Keeps <yusuftezcan@gmail.com>
2023-10-05 16:53:40 +0000

    updates

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Yusuf TEZCAN",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "nactez | Senior Security Engineer",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "Cybersecurity Blog",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
